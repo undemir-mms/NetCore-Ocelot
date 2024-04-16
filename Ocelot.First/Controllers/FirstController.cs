46d72aa4f9058c15083ac1307ac136ed2285869e
undemir-mms <166118231+undemir-mms@users.noreply.github.com>
2024-04-16 10:34:52 +0300

    Proje dosyası ekle.

---
+++ Ocelot.First/Controllers/FirstController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ocelot.First.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet("Get")]
+        public string Get()
+        {
+            return "First Api Work";
+        }
+    }
+}
---
+++ Ocelot.Second/Controllers/SecondController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ocelot.Second.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecondController : ControllerBase
+    {
+        [HttpGet("Get")]
+        public string Get()
+        {
+            return "Second Api Work";
+        }
+    }
+}
